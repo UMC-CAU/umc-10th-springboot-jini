8678ede5bcaa835227316c34e0969fd1001c4fea
gigajini <qetuoaldjg@gmail.com>
2026-05-06 17:42:11 +0900

    feat: review도메인 엔티티 제작 및 매핑

--- src/main/java/com/example/jini_umc10th/domain/review/entity/Review.java
+++ src/main/java/com/example/jini_umc10th/domain/review/entity/Review.java
@@ -1,4 +1,41 @@
 package com.example.jini_umc10th.domain.review.entity;
 
-public class Review {
+import com.example.jini_umc10th.domain.member.entity.Member;
+import com.example.jini_umc10th.domain.mission.entity.Restaurant;
+import com.example.jini_umc10th.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "restaurant_review")
+public class Review extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "restaurant_review_id")
+    private Long id;
+
+    @Column(name = "rating", nullable = false, precision = 2, scale = 1)
+    private BigDecimal rating;
+
+    @Column(name = "review_content", nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_id", nullable = false)
+    private Restaurant restaurant;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id", nullable = false)
+    private Member member;
+
 }
--- src/main/java/com/example/jini_umc10th/domain/review/entity/ReviewReply.java
+++ src/main/java/com/example/jini_umc10th/domain/review/entity/ReviewReply.java
@@ -1,4 +1,29 @@
 package com.example.jini_umc10th.domain.review.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "review_reply")
 public class ReviewReply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "review_reply_id")
+    private Long id;
+
+    @Column(name = "reply_content", nullable = false, length = 500)
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "restaurant_review_id", nullable = false)
+    private Review review;
+
 }
